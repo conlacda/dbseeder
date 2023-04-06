58b94c8d4f73d64da4fec4a761d381f2527c3e25
conlacda <conlacda@users.noreply.github.com>
2023-04-06 12:11:02 +0000

    Apply black changes

--- fields.py
+++ fields.py
@@ -66,7 +66,7 @@ class Field:
     data_type: str = ""
     nullable: bool = True
     default_value: str = ""
-    key: str = "" # PRI (primary), UNI (unique)
+    key: str = ""  # PRI (primary), UNI (unique)
     max: int = 0
     min: int = 0
 
